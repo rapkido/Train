fb941ad9baec6ea69b0d7771d126b44c3b94d58e
rapkido <coskuner.bestami@gmail.com>
2019-03-17 01:55:27 +0300

    You're doing great

---
+++ 003-strings/slices-bytes.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "Hello, playground"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	bs := []byte(s)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%#U ", s[i])
+	}
+}
