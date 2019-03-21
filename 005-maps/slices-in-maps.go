9a4faef834744079a034a0ea11c320959e263b60
rapkido <coskuner.bestami@gmail.com>
2019-03-21 15:56:01 +0300

    You're doing great

---
+++ 005-maps/slices-in-maps.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	person := map[string][]string{
+		"bond_james":      {"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": {"moneypenny_miss", "James Bond", "Literature", "Computer Science"},
+		"no_dr":           {"Being evil", "Ice cream", "Sunsets"},
+	}
+	for i, per := range person {
+		fmt.Printf("Kayıt: %v\n", i)
+		for j, data := range per {
+			fmt.Printf("\t%v. içerik: %v\n", j, data)
+		}
+
+	}
+}
+
